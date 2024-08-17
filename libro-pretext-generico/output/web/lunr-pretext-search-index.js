0529009b7b8e09976de5747d489e78c2536571cd
Rommel Contreras <rommeljose@gmail.com>
2024-08-17 09:47:59 -0400

    ok

--- libro-pretext-generico/output/web/lunr-pretext-search-index.js
+++ libro-pretext-generico/output/web/lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Colofón",
   "number": "",
   "title": "Colofón",
-  "body": "  Diseño de la Portada  AUTOR DE LA PORTADA    Editor Productor  NOMBRE DEL EDITOR O DEL AUTOR   1 edición digital, 2023    TÍTULO DEL LIBRO o DE LA PÁGINA WEB DEL LIBRO   https:\/\/efemeridescumana.blogspot.com\/   copyright  "
+  "body": "  Diseño de la Portada  AUTOR DE LA PORTADA    Editor Productor  NOMBRE DEL EDITOR O DEL AUTOR   1 edición digital, 2023   TÍTULO DEL LIBRO o DE LA PÁGINA WEB DEL LIBRO   copyright  "
 },
 {
   "id": "author-biografia",
@@ -19,9 +19,9 @@ var ptx_lunr_docs = [
   "body": "  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.     "
 },
 {
-  "id": "p-1",
+  "id": "author-biografia-1",
   "level": "2",
-  "url": "author-biografia.html#p-1",
+  "url": "author-biografia.html#author-biografia-1",
   "type": "párrafo (with a defined term)",
   "number": "",
   "title": "",
@@ -40,9 +40,9 @@ var ptx_lunr_docs = [
   "id": "reconocimiento",
   "level": "1",
   "url": "reconocimiento.html",
-  "type": "Agradecimentos",
+  "type": "Agradecimientos",
   "number": "",
-  "title": "Agradecimentos",
+  "title": "Agradecimientos",
   "body": " TEXTO DE LOS RECONOCIMIENTOS  "
 },
 {
@@ -55,27 +55,27 @@ var ptx_lunr_docs = [
   "body": " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porta et est vel rhoncus. Morbi fermentum magna sed lorem interdum rhoncus. Phasellus vitae ligula ut ante posuere tincidunt et eget libero. Ut imperdiet laoreet lacinia. Praesent in lectus et nisi fermentum placerat. Morbi fermentum orci neque, vel pulvinar lectus molestie quis. Curabitur condimentum, erat ac mollis tempor, risus justo pulvinar arcu, sit amet pellentesque ante justo quis neque. Cras blandit elit sit amet lorem feugiat malesuada. Sed molestie leo sed placerat suscipit.   La verdad es que el dolor en sí es importante, y a él le sigue la educación del alumno, pero al mismo tiempo sucede que hay un gran dolor y trabajo. Porque llegaré al fondo, quien no ejerce ningún esfuerzo. tipo de trabajo a menos que obtenga algún beneficio de ello. Dous o irrure dolor en el placer reprende que quiere ser pestaña del dolor, que huya de él, y nadie dará a luz. A menos que estén cegados por el deseo, no no salen, tienen culpa los que abandonan sus deberes, el alma se ablanda, es decir se afana.        INICIALES O NOMBRE DEL AUTOR  FECHA DEL PREFACIO   "
 },
 {
-  "id": "nombre.seccion",
+  "id": "nombre-seccion",
   "level": "1",
-  "url": "nombre.seccion.html",
+  "url": "nombre-seccion.html",
   "type": "Sección",
   "number": "1.1",
   "title": "NOMBRE DE LA SECCIÓN.",
   "body": " NOMBRE DE LA SECCIÓN.   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fringilla justo erat, a tincidunt mauris congue quis. Vivamus quis massa erat. Suspendisse cursus vehicula purus, in consectetur ante maximus sed. Cras in tristique nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed lacinia, nunc at viverra efficitur, odio arcu placerat dui, at finibus nibh odio at enim. Mauris nibh purus, malesuada non faucibus ut, volutpat vel nibh. Curabitur et quam vel urna tincidunt viverra in at turpis. Maecenas commodo vehicula enim, congue porta odio pharetra nec. Aenean sed dignissim sapien. Morbi scelerisque commodo convallis. Nulla facilisi. Nulla volutpat libero erat, consectetur mattis mauris vulputate eget. Nunc efficitur velit sapien, sit amet venenatis tortor iaculis eget. Duis scelerisque a risus in viverra.   Donec facilisis ex scelerisque molestie egestas. Nam interdum aliquet leo id feugiat. Vestibulum arcu sem, blandit eget vehicula nec, iaculis nec dolor. Curabitur turpis mi, scelerisque quis nibh congue, placerat hendrerit elit. Phasellus condimentum, leo sit amet suscipit varius, tellus diam blandit dui, dapibus placerat nisl libero in nibh. Suspendisse volutpat vitae sapien nec ultricies. Praesent vitae nisl consectetur, accumsan velit non, volutpat turpis. Aenean eu ipsum mauris. Donec turpis justo, commodo vel volutpat et, viverra vitae massa. Duis sit amet velit quis eros consectetur accumsan vitae non ligula.       "
 },
 {
-  "id": "references-1",
+  "id": "backmatter-1",
   "level": "1",
-  "url": "references-1.html",
+  "url": "backmatter-1.html",
   "type": "Referencias",
   "number": "",
   "title": "Bibliografía:",
   "body": " Bibliografía:  Sender, Ramón J. Proclamación de la sonrisa.  Impr. Juan Pueyo. Madrid . 1934 .  Casas, Fray Bartolomé de las. Historia General de las Indias.  Editado por Archivo General de Indias. copia manuscrita del original (Chiapas: AGI, 1527-1561) . 1561? .  "
 },
 {
-  "id": "colophon-2",
+  "id": "backmatter-2",
   "level": "1",
-  "url": "colophon-2.html",
+  "url": "backmatter-2.html",
   "type": "Colofón",
   "number": "",
   "title": "Colofón",
@@ -87,6 +87,7 @@ var ptx_lunr_idx = lunr(function () {
   this.ref('id')
   this.field('title')
   this.field('body')
+  this.metadataWhitelist = ['position']
 
   ptx_lunr_docs.forEach(function (doc) {
     this.add(doc)
